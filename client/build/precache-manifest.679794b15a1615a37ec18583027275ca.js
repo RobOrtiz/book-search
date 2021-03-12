2a8651b7f8e0b4abc5ee2f6e6545d18caabadd88
RobOrtiz <mrrobortiz@gmail.com>
2021-03-12 08:53:54 -0800

    build

---
+++ client/build/precache-manifest.679794b15a1615a37ec18583027275ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3d9e0898dc48ebade9a",
+    "url": "/static/css/main.5df8ae2b.chunk.css"
+  },
+  {
+    "revision": "e3d9e0898dc48ebade9a",
+    "url": "/static/js/main.5df0a259.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c209672c7f9e0b87dbf",
+    "url": "/static/js/2.c4dbebd7.chunk.js"
+  },
+  {
+    "revision": "6529a04a4600dbab9a149995f9bb9e66",
+    "url": "/index.html"
+  }
+];
